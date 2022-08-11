eb62b0afffe9c51b7645b79641f4e7cd053f1ab8
Alexandru Alexa <x.alx@yahoo.com>
2022-08-11 01:59:26 -0800

    add listAll() method in CustomerService

--- src/main/java/com/aa/service/CustomerServiceImpl.java
+++ src/main/java/com/aa/service/CustomerServiceImpl.java
@@ -1,5 +1,22 @@
 package com.aa.service;
 
-public class CustomerServiceImpl implements CustomerService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aa.dao.CustomerDAO;
+import com.aa.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	public List<Customer> listAll() {
+		List<Customer> listCustomers = customerDAO.findAll();
+		
+		return listCustomers;
+	}
 }
