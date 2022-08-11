7e8ee9c3f9846525522304498597d99b41746c7b
Alexandru Alexa <x.alx@yahoo.com>
2022-08-11 00:07:53 -0800

    Add DAO

---
+++ src/main/java/com/aa/dao/CustomerDAO.java
@@ -0,0 +1,5 @@
+package com.aa.dao;
+
+public interface CustomerDAO {
+
+}
---
+++ src/main/java/com/aa/dao/CustomerDAOImpl.java
@@ -0,0 +1,8 @@
+package com.aa.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+}
